a1d12d2c13b572a298b42728987f39763c40cff2
Marton <mpochavillo@gmail.com>
2018-03-10 15:06:24 +0800

    continued two rows

--- wp-content/themes/my-portfolio/front-page.php
+++ wp-content/themes/my-portfolio/front-page.php
@@ -5,8 +5,8 @@ get_header(); ?>
 
 <!-- row One -->
 
-<div class="page-section">
-	<dir class="wrapper">
+<div class="page-section page-section--red">
+	<div class="wrapper">
 		<h1 class="headline headline__title">Title here</h1>
 		<p class="headline headline__content">
 			“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
@@ -15,121 +15,122 @@ get_header(); ?>
 			<h1 class="headline headline__title">Second TItle</h1>
 			<p class="headline headline__content">
 			“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-	</dir>
+	</div>
 </div>
+
+
+
 
 
 <!-- row Two -->
 
-<div class="page-section">
-			<div class="wrapper wrapper--colored">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="gtco-heading">
-							<h2 class="gtco-left">Services</h2>
-							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-						</div>
-					</div>
+<div class="page-section">   <!-- all 8 are in this div -->
+	<div class="wrapper">
+				<div class="row"> <!-- this is for the header of 2 columns -->		
+					<h2 class="headline headline__title">Services</h2>
+					<p class="headline headline__content">Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>	
 				</div>
-				<div class="row">
-					<div class="col-md-6">
-						<div class="row">
 
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+				
+
+				<div class="row row--gutters">
+					
+
+					<!-- column one -->
+					<div class="row--6">
+							
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-address-book"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Web Development</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Web Development</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+								
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-align-justify"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Web Design</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Web Design</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+								
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-anchor"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Customer Support</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Customer Support</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+								
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-fire"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Web Design</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Web Design</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-						</div>
+								
 					</div>
-					<div class="col-md-6 animate-box" data-animate-effect="fadeIn">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+
+					<div class="row--6">
+						<!--column two -->
+						
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-medkit"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Copy Writing</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Copy Writing</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+							
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-address-card"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>CMS Development</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">CMS Development</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+								
+							
+									
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-arrows-alt"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Data Gathering</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Data Gathering</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="feature-left">
-									<span class="icon">
+							
+							
+									<div class="feature">
+										<span class="feature__icon">
 										<i class="fa fa-backward"></i>
 									</span>
-									<div class="feature-copy">
-										<h3>Search Engine</h3>
-										<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+										<h3 class="feature__title">Search Engine</h3>
+										<p class="headline__content headline__narrow">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
 									</div>
-								</div>
-							</div>
-						</div>
+								
 					</div>
 				</div>
+					
+
+					
+
+					
+				
 			</div>
 		</div>
 
